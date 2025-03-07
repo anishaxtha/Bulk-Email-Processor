48fbf81bc99977b7349872a827143dec80b49a4d
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-07 13:45:50 +0545

    user model

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // app middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 // db connection
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: [50, "Name cannot be more then 20 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please provide an email"],
+      unique: true,
+      match: [
+        /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/,
+        "Please provide a valid email",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: [6, "Password must be at least 6 characters"],
+      select: false,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: String,
+    verificationTokenExpire: Date,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", UserSchema);

f9dbdf86d091fb568a6fd6743a9c624f709aeed3
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-07 21:58:54 +0545

    user models

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -36,4 +36,36 @@ const UserSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Encrypt password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Generate JWT Token
+UserSchema.methods.generateAuthToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRATION,
+  });
+};
+
+// Match password
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate email verification token
+UserSchema.methods.generateVerificationToken = function () {
+  const verificationToken =
+    Math.random().toString(36).substring(2, 15) +
+    Math.random().toString(36).substring(2, 15);
+
+  this.verificationToken = verificationToken;
+  this.verificationTokenExpire = Date.now() + 24 * 60 * 60 * 1000; 
+  return verificationToken;
+};
+
 module.exports = mongoose.model("User", UserSchema);

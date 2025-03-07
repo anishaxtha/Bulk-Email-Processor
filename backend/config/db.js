c62f73dbd91fa7b07a6e71a90af90d92821fc4aa
Anisha Nayaju <anishanayaju26@gmail.com>
2025-03-07 12:17:27 +0545

    database connected

---
+++ backend/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("Database connected successfully ");
+  } catch (error) {
+    console.error("Error:", error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,26 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+dotenv.config();
 
 //app config
 const app = express();
-const port = 4005;
+const PORT = process.env.PORT || 5000;
 
 // app middleware
 app.use(express.json());
 app.use(cors());
+
+// db connection
+connectDB();
 
 // checking the end points
 app.get("/", (req, res) => {
   res.send("Hello from api...");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-  console.log(`Browse to http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Browse to http://localhost:${PORT}`);
 });
